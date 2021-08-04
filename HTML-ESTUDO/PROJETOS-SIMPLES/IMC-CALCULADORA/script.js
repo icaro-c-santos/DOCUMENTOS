88919f11ecb89924e141dbed6cccad2e44bff4d6
icaro-c-santos <icarosalna@hotmail.com>
2021-08-03 22:47:50 -0300

    UPDATE
    UP

--- HTML-ESTUDO/PROJETOS-SIMPLES/IMC-CALCULADORA/script.js
+++ HTML-ESTUDO/PROJETOS-SIMPLES/IMC-CALCULADORA/script.js
@@ -1,13 +1,35 @@
 function imc(){
         
     var nome = document.getElementsByName("nome_usuario")[0].value.toUpperCase();
-    var idade = document.getElementsByName("idade_usuario")[0].value;
-    var altura = document.getElementsByName("altura_usuario")[0].value;
-    var peso = document.getElementsByName("peso_usuario")[0].value;
-    var sexoF = document.getElementsByName("sexo_usuarioF")[0];
-    var sexoM = document.getElementsByName("sexo_usuarioM")[0];
+    if(!nome){ alert("ERRO! PREENCHA O CAMPO NOME"); return 0;}
 
-    var indice = (peso/(altura**2));
+    var idade = document.getElementsByName("idade_usuario")[0].value;
+    if(!idade){ alert("ERRO! PREENCHA O CAMPO IDADE"); return 0;} 
+
+    var sexo = document.getElementsByName("sexo_usuario")[0].value;
+    if(!sexo){ alert("ERRO! PREENCHA O CAMPO SEXO"); return 0;} //SEXO NÃO TEM SERVENTIA SO COLOQUEI POR APRENDIZADO DO USO DOS INPUTS.
+
+    var altura = document.getElementsByName("altura_usuario")[0].value;
+    if(!altura || altura ==="0"){alert("ERRO! PREENCHA O CAMPO ALTURA"); return 0;}
+    if(typeof(altura)==="string"){
+       altura = altura.replace(',','.');
+    }
+    if(isNaN(altura)){
+       alert("ERRO! PREENCHA O CAMPO ALTURA SOMENTE COM NUMEROS E PONTO EX: 1.98" ); 
+       return 0;
+    }
+
+    var peso = document.getElementsByName("peso_usuario")[0].value;
+    if(!peso){ alert("ERRO! PREENCHA O CAMPO PESO"); return 0;}
+    if(typeof(peso)==="string"){
+        peso = peso.replace(',','.');
+     }
+     if(isNaN(peso)){
+        alert("ERRO! PREENCHA O CAMPO PESO SOMENTE COM NUMEROS E PONTO EX: 1.98" ); 
+        return 0;
+     }
+
+    var indice = (peso/(altura*altura));
     var resultado;
 
     var div_resultado =window.document.querySelector('div.result');
@@ -17,33 +39,40 @@ function imc(){
 
     var div_resposta2 = window.document.getElementsByClassName("resultado_nome")[0];
 
-    if(sexoF.checked ==false && sexoM.checked == false){
-           alert("ERRO! ESCOLHE UMA OPÇÃO DE SEXO!");
-           fechar();
+     if(idade > 65){ indice-=5 }else{
+        if(idade >= 55){ indice-=4 }else{
+            if(idade >= 45){ indice-=3 }else{
+                if(idade >= 35){ indice-=2 }else{
+                    if(idade >= 25){ indice-=1 }
+                }
+            }
+        }
     }
-    
-  if(sexoF.checked ==true){
-      if(indice>=42){
-          resultado = "OBESIDADE MORBIDA";
-      }if(indice>=37){
-          resultado = "OBESIDADE MORBIDA GRAU 2";
-      }if(indice>=32){
-          resultado = "OBESIDADE MORBIDA GRAU 1";
-      }if(indice>=27){
-          resultado = "LEVEMENTE ACIMDA DO PESO";
-      }if(indice>=22){
-          resultado = "PESO IDEAL";
-      }if(indice<22){
-          resultado = "SUBNUTRIDO/ABAIXO DO PESO";
+  if(sexo === "MASCULINO" || sexo === "FEMININO"){
+      if(indice>=39){ resultado = "OBESIDADE GRAVE"; }else{
+        if(indice>=29){ resultado = "OBESIDADE"; }else{
+            if(indice>=24){ resultado = "PRÉ OBESIDADE"; }else{
+                if(indice>=19){ resultado = "PESO NORMAL"; }else{
+                    if(indice<19){
+                        resultado = "PESO BAIXO";
+                    }
+                }
+
+            }
+        }
       }
   }
   
   div_resposta2.innerText = `${nome} SEU RESULTADO É:`;
   div_resposta.innerText = resultado;
-
+  setTimeout(3000);
   }
 
   function fechar(){
     var div_resultado =window.document.querySelector('div.result');
     div_resultado.style.display="none";
+  }
+  
+
+      
   }
---
+++ teste.js
@@ -0,0 +1,6 @@
+var altura = "1.1";
+if(){
+    console.log("somente numeros")
+}else{
+    console.log("LETRAS")
+}
