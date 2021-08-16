8f2dc8dab2d618f94d39777e1bead445f8539e11
icaro-c-santos <icarosalna@hotmail.com>
2021-08-15 21:12:07 -0300

    ok

--- teste.js
+++ teste.js
@@ -1,10 +1,4 @@
-var nome = "tudo bem";
-nome.split();
-for (const key in nome) {
-        const element = object[key];
-        
-    }
-}
-console.log(nome);
 
-
+var lista = ['programação','mobile','profissional','perfomace','prodigio','produção'];
+var palavra = 'pro';
+console.log(lista.filter(p => p.includes(palavra)))
