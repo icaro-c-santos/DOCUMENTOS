475939d316bc7b4aab240078e85608c3758af48e
icaro-c-santos <icarosalna@hotmail.com>
2021-08-24 14:53:49 -0300

    update

--- teste/server.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.listen(8001,() =>console.log("SEI DE NADA!"));
