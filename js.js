2601bba51a08e6cef45e858483fa314a11174ceb
icaro-c-santos <icarosalna@hotmail.com>
2021-08-26 14:33:01 -0300

    update

--- js.js
+++ js.js
@@ -1,23 +1,8 @@
-class aluno {
-
-    nome = null;
-    nota = null;
-
-    constructor(nome,nota){
-        this.nome = nome;
-        this.nota = nota;
-    }
-    
-    getnota(){
-        return this.nota;
-    }
-    getnome(){
-        return this.nome;
-    }
-    isEquals(aluno_1,alunos_2){
-      return  aluno_1.nota - alunos_2.nota;
-    }
-
+function saudar(){
+    return "OLA";
+};
+function  bomdia(funcao) {
+   console.log(funcao);
 }
 
-module.exports = aluno;
+bomdia(saudar());
