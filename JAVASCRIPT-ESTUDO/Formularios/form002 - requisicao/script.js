1ffb60249a80de1bd7de05794cf0be760917c8ae
ICARO <icarosalna@hotmail.com>
2021-10-30 22:04:36 -0300

    UPDATE

---
+++ JAVASCRIPT-ESTUDO/Formularios/form002 - requisicao/script.js
@@ -0,0 +1,10 @@
+function cadastro_pessoa(){
+    
+
+    const form = document.querySelector("#form_pessoa"); ///obtem o formulario que queremos pegar os dados.
+
+    const formData = new FormData(form); ///obtem os dados do formulario
+
+    
+    
+}
