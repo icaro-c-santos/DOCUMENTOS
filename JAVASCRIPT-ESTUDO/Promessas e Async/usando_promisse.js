fd4227cf9b87e77f1052f31188f4374149729aeb
ICARO <icarosalna@hotmail.com>
2022-02-11 21:20:03 -0300

    up

--- JAVASCRIPT-ESTUDO/Promessas e Async/usando_promisse.js
+++ JAVASCRIPT-ESTUDO/Promessas e Async/usando_promisse.js
@@ -4,22 +4,6 @@
 
 function buscar(nome){
 
-    const p = new Promise((resolve,reject)=>{
-
-    
-        try{
-            if(Math.floor(Math.random() * 11)<5){
-                resolve(`TUDO CERTO! ${nome} FOI ENCONTRADO!`);
-            }else{
-                throw ("ALGO DEU ERRADO!");
-            }
-            
-        }catch(e){
-            reject(e);
-        }
-
-    });
-
     p.then(console.log).catch(console.log);
 
 }
